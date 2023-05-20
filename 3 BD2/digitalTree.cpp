cecebe83b7e8cc75c395c8ca46d44eb1a0f32a3a
croko22 <kev132011@hotmail.com>
2023-05-20 12:27:23 -0500

    BD2 - DigitalTree (insert, get, pending delete)

--- 3 BD2/digitalTree.cpp
+++ 3 BD2/digitalTree.cpp
@@ -1,117 +1,97 @@
 #include <iostream>
+#include <array>
 #include <string>
+#include <memory>
 using namespace std;
-#define N 128
+constexpr int N = 26;
 
-typedef struct DTree DTree;
-// TODO: Terminar el arbol como en la imagen que siga la key y eso es algo como un trie
-// https://gist.github.com/datlife/70607579c7377ad9f4e86bc5d1d81f41
-// https://leetcode.com/problems/implement-trie-prefix-tree/solutions/1626293/modern-c-trie-solution/
-struct DTree
+class DTree
 {
-    char data;
-    DTree *children[N];
-    int is_leaf;
+public:
+    DTree() : root(std::make_unique<Node>('\0')) {}
+    void insert(string word, int data = 0)
+    {
+        auto current = root.get();
+
+        for (auto &c : word)
+        {
+            const int index = c - 'a';
+            auto &child = current->children.at(index);
+
+            if (child == nullptr)
+            {
+                child = std::make_unique<Node>(c);
+            };
+
+            current = child.get();
+        }
+        current->isWord = true;
+        current->data = data;
+    }
+
+    int search(string word)
+    {
+        auto node = getNode(word);
+        // TESTING
+        // cout << node->data << endl;
+        // return node != nullptr && node->isWord;
+        if (node != nullptr && node->isWord)
+        {
+            return node->data;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    bool startsWith(string prefix)
+    {
+        return getNode(prefix) != nullptr;
+    }
+
+private:
+    class Node
+    {
+    public:
+        Node(char ch = '\0') : c(ch){};
+        const char c = '\0';
+        std::array<std::unique_ptr<Node>, N> children;
+        // buscar parte de la palabra
+        bool isWord = false;
+        int data;
+    };
+
+    Node *getNode(string word)
+    {
+        auto current = root.get();
+        for (auto &c : word)
+        {
+            const int index = c - 'a';
+
+            auto child = current->children.at(index).get();
+            // if null, return null,
+            if (child == nullptr)
+                return nullptr;
+            // continuar
+            current = child;
+        }
+        return current;
+    }
+    std::unique_ptr<Node> root;
 };
-
-DTree *make_DTree(char data)
-{
-    DTree *node = (DTree *)calloc(1, sizeof(DTree));
-    for (int i = 0; i < N; i++)
-        node->children[i] = NULL;
-    node->is_leaf = 0;
-    node->data = data;
-    return node;
-}
-
-void free_DTree(DTree *node)
-{
-    for (int i = 0; i < N; i++)
-    {
-        if (node->children[i] != NULL)
-        {
-            free_DTree(node->children[i]);
-        }
-        else
-            continue;
-    }
-    delete node;
-}
-
-DTree *insert(DTree *root, char *word)
-{
-    DTree *temp = root;
-
-    for (int i = 0; word[i] != '\0'; i++)
-    {
-        int idx = (int)word[i] - 'a';
-        if (temp->children[idx] == NULL)
-        {
-            temp->children[idx] = make_DTree(word[i]);
-        }
-        else
-        {
-        }
-        temp = temp->children[idx];
-    }
-    temp->is_leaf = 1;
-    return root;
-}
-
-int search_trie(DTree *root, char *word)
-{
-    DTree *temp = root;
-    for (int i = 0; word[i] != '\0'; i++)
-    {
-        int position = word[i] - 'a';
-        if (temp->children[position] == NULL)
-            return 0;
-        temp = temp->children[position];
-    }
-    if (temp != NULL && temp->is_leaf == 1)
-    {
-        cout << temp->data << endl;
-        return 1;
-    }
-    return 0;
-}
-
-void print_search(DTree *root, char *word)
-{
-    printf("Searching for %s: ", word);
-    if (search_trie(root, word) == 0)
-        printf("Not Found\n");
-    else
-        printf("Found!\n");
-}
-
-void print_trie(DTree *root)
-{
-    if (!root)
-        return;
-    DTree *temp = root;
-    cout << temp->data << " -> ";
-    for (int i = 0; i < N; i++)
-    {
-        print_trie(temp->children[i]);
-    }
-}
 
 int main()
 {
-    // Raiz del arbol
-    DTree *root = make_DTree('\0');
-    // Inserciones
-    root = insert(root, "uno");
-    root = insert(root, "union");
-    root = insert(root, "siete");
-    root = insert(root, "sietemil");
+    DTree *dtree = new DTree();
+    dtree->insert("uno", 1);
+    dtree->insert("union", 2);
+    dtree->insert("siete", 3);
+    dtree->insert("sietemil", 4);
     cout << endl;
-    print_trie(root);
-    cout << endl;
-    print_search(root, "uuu");
-    print_search(root, "uno");
-    print_search(root, "sietemil");
-    free_DTree(root);
+    cout << dtree->search("uuu") << endl;
+    cout << dtree->search("uno") << endl;
+    cout << dtree->search("sietemil") << endl;
+    // cout << dtree->startsWith("gn") << endl;
     return 0;
 }
