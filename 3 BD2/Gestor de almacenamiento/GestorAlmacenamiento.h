f997cc58ad8c8da3d806b65cd47f238d5dcddb37
croko22 <kev132011@hotmail.com>
2023-06-16 12:37:11 -0500

    Gestor de almacenamiento project setup

---
+++ 3 BD2/Gestor de almacenamiento/GestorAlmacenamiento.h
@@ -0,0 +1,17 @@
+class GestorAlmacenamiento
+{
+private:
+    vector<string> paginas;
+
+public:
+    GestorAlmacenamiento(/* args */);
+    ~GestorAlmacenamiento();
+};
+
+GestorAlmacenamiento::GestorAlmacenamiento(/* args */)
+{
+}
+
+GestorAlmacenamiento::~GestorAlmacenamiento()
+{
+}
--- 3 BD2/Gestor de almacenamiento/main.cpp
+++ 3 BD2/Gestor de almacenamiento/main.cpp
@@ -6,19 +6,29 @@ using namespace std;
 const int BUFFER_SIZE = 1024;
 std::vector<char> buffer(BUFFER_SIZE + 1, 0);
 
+// TODO: Useful links
+//  https://opendsa-server.cs.vt.edu/OpenDSA/Books/PIFLAS21/html/index.html
+//  https://github.com/krshrimali/CPP-File-Manager/blob/master/src/FileManager.cpp
+//  https://gist.github.com/andschwa/d120be76ba8ebd66cf50
+//  https://codereview.stackexchange.com/questions/270928/reading-from-large-binary-files
+//  https://cplusplus.com/doc/tutorial/files/
+//  https://stackoverflow.com/questions/20911584/how-to-read-a-file-in-multiple-chunks-until-eof-c
+
 int main(int argc, char const *argv[])
 {
-    streampos begin, end;
-    // ofstream myfile("filename.txt");
-    ifstream myfile("filename.txt");
-    // myfile << "Files can be tricky, but it is fun enough!";
+    std::ifstream file("filename.txt", std::ios::binary | std::ios::ate);
+    std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
 
-    begin = myfile.tellg();
-    myfile.seekg(0, ios::end);
-    end = myfile.tellg();
+    std::vector<char> buffer(size);
+    if (file.read(buffer.data(), size))
+    {
+        std::cout << "read " << size << " bytes\n";
+    }
+    else
+    {
+        std::cout << "error: only " << file.gcount() << " could be read\n";
+    }
 
-    myfile.close();
-
-    cout << "size is: " << (end - begin) << " bytes.\n";
     return 0;
 }
