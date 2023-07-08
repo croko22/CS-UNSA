cdef2f02aa5cab635887363957ba790dc213c5f5
croko22 <kev132011@hotmail.com>
2023-07-08 11:41:35 -0500

    BufferPMgr - splited to class, implemented LRU & MRU

--- 3 BD2/3 - Buffer Manager/BufferPoolManager.cpp
+++ 3 BD2/3 - Buffer Manager/BufferPoolManager.cpp
@@ -1,1 +1,100 @@
-// TODO: Cuando termines ordena las clases y funciones
+#include "BufferPoolManager.h"
+
+BufferPoolManager::BufferPoolManager(std::shared_ptr<GestorAlmacenamiento> gestor)
+    : gestor(gestor), strategy("LRU")
+{
+}
+
+Page *BufferPoolManager::FetchPage(int page_id)
+{
+    for (auto it = pages.begin(); it != pages.end(); ++it)
+    {
+        if (it->get_page_id() == page_id)
+        {
+            std::rotate(pages.begin(), it, it + 1);
+            return &pages.front();
+        }
+    }
+
+    Page new_page(gestor, page_id);
+    new_page.subprocess_count++;
+
+    if (strategy == "LRU")
+    {
+        pages.back() = new_page;
+        return &pages.back();
+    }
+    if (strategy == "MRU")
+    {
+        pages.front() = new_page;
+        return &pages.front();
+    }
+
+    return &pages.front();
+}
+
+void BufferPoolManager::NewPage(int page_id)
+{
+    pages.push_back(Page(gestor, page_id));
+}
+
+bool BufferPoolManager::UnpinPage(int page_id, bool is_dirty)
+{
+    for (Page &page : pages)
+    {
+        if (page.get_page_id() == page_id)
+        {
+            page.subprocess_count--;
+
+            return true;
+        }
+    }
+    return false;
+}
+
+bool BufferPoolManager::FlushPage(int page_id)
+{
+    for (Page &page : pages)
+    {
+        if (page.get_page_id() == page_id)
+        {
+            if (page.dirty)
+            {
+                gestor->escribirBloque(page_id, page.buffer);
+                std::cout << "Page " << page_id << " was written" << std::endl;
+            }
+            return true;
+        }
+    }
+    return false;
+}
+
+void BufferPoolManager::FlushAllPages()
+{
+    for (Page &page : pages)
+    {
+        if (page.dirty)
+        {
+            gestor->escribirBloque(page.get_page_id(), page.buffer);
+            std::cout << "Page " << page.get_page_id() << " was written" << std::endl;
+        }
+    }
+}
+
+void BufferPoolManager::DeletePage(int page_id)
+{
+    pages.erase(std::remove_if(pages.begin(), pages.end(), [&](Page &page)
+                               { return page.get_page_id() == page_id; }),
+                pages.end());
+}
+
+void BufferPoolManager::PrintFrames()
+{
+    int count{0};
+    std::cout << "Frames\tPageID\tCount\tDirty" << std::endl;
+    for (Page &page : pages)
+    {
+        count++;
+        std::cout << count << "\t" << page.get_page_id() << "\t" << page.subprocess_count << "\t" << page.dirty << std::endl;
+    }
+}
---
+++ 3 BD2/3 - Buffer Manager/BufferPoolManager.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "GestorAlmacenamiento.h"
+#include "Page.h"
+
+class BufferPoolManager
+{
+private:
+    std::shared_ptr<GestorAlmacenamiento> gestor;
+    std::vector<Page> pages;
+    std::function<void()> replacement_strategy;
+    std::string strategy;
+
+public:
+    BufferPoolManager(std::shared_ptr<GestorAlmacenamiento> gestor);
+    Page *FetchPage(int page_id);
+    void NewPage(int page_id);
+    bool UnpinPage(int page_id, bool is_dirty);
+    bool FlushPage(int page_id);
+    void FlushAllPages();
+    void DeletePage(int page_id);
+    void PrintFrames();
+};
