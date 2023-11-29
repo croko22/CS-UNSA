31068631ed6f9af53ddf0cbb4132bf5d5f102e71
croko22 <kvchambit@gmail.com>
2023-11-29 17:10:15 -0500

    octree setup (still unclean)

---
+++ 4 EDA/LAB 3 - Octree/Cube1/OctTree.h
@@ -0,0 +1,29 @@
+struct Point
+{
+    int x, y, z;
+    Point(int x, int y, int z) : x(x), y(y), z(z) {}
+};
+
+class OctTree
+{
+private:
+    OctTree *children[8];
+    Point *points;
+
+    // bottomLeft
+    Point *bottomLeft;
+    double h;
+
+    int nPoints; // puntos en el cubo
+public:
+    OctTree();
+    bool exist(const Point *p);
+    void insert(const Point *p);
+    ~OctTree();
+};
+
+OctTree::OctTree(/* args */) {}
+
+OctTree::~OctTree()
+{
+}
---
+++ 4 EDA/LAB 3 - Octree/Cube1/main.cpp
@@ -0,0 +1,13 @@
+#include "OctTree.h"
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    OctTree *tree = new OctTree();
+    // Point *p = new Point(1, 2, 3);
+    // tree->insert(p);
+
+    cout << "HI" << endl;
+    return 0;
+}
