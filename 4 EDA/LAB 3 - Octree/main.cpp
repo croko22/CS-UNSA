6b0b6456cac2f0bdbc1a6cb21bd2aaeafa5fa7b7
croko22 <kvchambit@gmail.com>
2023-11-29 17:52:19 -0500

    added csv files

--- 4 EDA/LAB 3 - Octree/main.cpp
+++ 4 EDA/LAB 3 - Octree/main.cpp
@@ -1,5 +1,7 @@
 #include "OctTree.h"
 #include <iostream>
+#include <fstream>
+
 using namespace std;
 
 int main(int argc, char const *argv[])
@@ -8,6 +10,21 @@ int main(int argc, char const *argv[])
     // Point *p = new Point(1, 2, 3);
     // tree->insert(p);
 
-    cout << "HI" << endl;
+    fstream file;
+    file.open("points1.csv", ios::in);
+    if (file.is_open())
+    {
+        string line;
+        while (getline(file, line))
+        {
+            int x, y, z;
+            cout << line << endl; //* debug
+            sscanf(line.c_str(), "%d,%d,%d", &x, &y, &z);
+            Point *p = new Point(x, y, z);
+            tree->insert(p);
+        }
+    }
+
+    cout << "Insertados" << endl;
     return 0;
 }
