55cdc9cc12792c3f6366d21d0124444dc3a24540
croko22 <kvchambit@gmail.com>
2023-12-03 12:16:31 -0500

    change(main): reduce file size

--- 4 EDA/LAB 3 - Octree/main.cxx
+++ 4 EDA/LAB 3 - Octree/main.cxx
@@ -1,50 +1,8 @@
-#include <vtkCubeSource.h>
-#include <vtkPolyDataMapper.h>
-#include <vtkActor.h>
-#include <vtkRenderer.h>
-#include <vtkRenderWindow.h>
-#include <vtkNamedColors.h>
-#include <vtkRenderWindowInteractor.h>
-#include <vtkCamera.h>
-#include <vtkNew.h>
-#include <vtkProperty.h>
 #include <fstream>
 #include <string>
 #include "src/octree.h"
+#include "src/visualizer.h"
 using namespace std;
-
-void visualizeNode(Octree::Node *node, vtkRenderer *renderer)
-{
-  vtkNew<vtkNamedColors> colors;
-
-  if (!node)
-    return;
-
-  vtkSmartPointer<vtkCubeSource> cubeSource = vtkSmartPointer<vtkCubeSource>::New();
-  cubeSource->SetBounds(
-      static_cast<double>(node->point.x), static_cast<double>(node->point.x) + 10.0,
-      static_cast<double>(node->point.y), static_cast<double>(node->point.y) + 10.0,
-      static_cast<double>(node->point.z), static_cast<double>(node->point.z) + 10.0);
-
-  vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-  mapper->SetInputConnection(cubeSource->GetOutputPort());
-
-  vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
-  actor->SetMapper(mapper);
-  actor->GetProperty()->SetColor(colors->GetColor3d("Tomato").GetData());
-
-  renderer->AddActor(actor);
-
-  for (int i = 0; i < 8; ++i)
-  {
-    visualizeNode(node->children[i], renderer);
-  }
-}
-
-void visualizeOctree(Octree *octree, vtkRenderer *renderer)
-{
-  visualizeNode(octree->root, renderer);
-}
 
 int main(int argc, char const *argv[])
 {
@@ -70,28 +28,10 @@ int main(int argc, char const *argv[])
       octree->insert(p);
     }
   }
-  cout << "Insertados" << endl;
 
-  vtkNew<vtkRenderer> ren;
-  vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
-  renderWindow->AddRenderer(ren);
-  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
-  renderWindowInteractor->SetRenderWindow(renderWindow);
-
-  visualizeOctree(octree, ren);
-
-  ren->ResetCamera();
-  ren->GetActiveCamera()->Azimuth(30);
-  ren->GetActiveCamera()->Elevation(30);
-  ren->ResetCameraClippingRange();
-  ren->SetBackground(colors->GetColor3d("Silver").GetData());
-
-  renderWindow->SetSize(1200, 800);
-  renderWindow->SetWindowName("OCTREE");
-
-  renderWindowInteractor->Initialize();
-  renderWindow->Render();
-  renderWindowInteractor->Start();
+  Visualizer visualizer;
+  visualizer.visualizeOctree(octree);
+  visualizer.render();
 
   delete octree;
 
