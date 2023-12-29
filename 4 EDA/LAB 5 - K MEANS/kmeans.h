b0edcb8d93840db74bdf6ac3954c9b11248306a3
croko22 <kvchambit@gmail.com>
2023-12-29 09:17:16 -0500

    reafctor of kmeans

--- 4 EDA/LAB 5 - K MEANS/kmeans.h
+++ 4 EDA/LAB 5 - K MEANS/kmeans.h
@@ -3,11 +3,6 @@
 #include "kdtree.h"
 using namespace std;
 
-// double distance(const Point &a, const Point &b)
-// {
-//     return std::sqrt(std::pow(a[0] - b[0], 2) + std::pow(a[1] - b[1], 2));
-// }
-
 double distance(const Point &p1, const Point &p2)
 {
     double sum = 0.0;
@@ -18,6 +13,7 @@ double distance(const Point &p1, const Point &p2)
     }
     return std::sqrt(sum);
 }
+
 std::pair<std::vector<Point>, std::vector<std::vector<Point>>> kmeans(const std::vector<Point> &data, int k)
 {
     std::vector<Point> centroids(k);
@@ -86,68 +82,6 @@ std::pair<std::vector<Point>, std::vector<std::vector<Point>>> kmeans(const std:
     return {centroids, clusters};
 }
 
-// std::pair<std::vector<Point>, std::vector<std::vector<Point>>> kdkmeans(const std::vector<Point> &data, int k)
-// {
-//     // 1. Choose k random points as initial centroids
-//     std::vector<Point> centroids(k);
-//     std::vector<std::vector<Point>> clusters(k);
-//     for (int i = 0; i < k; ++i)
-//     {
-//         centroids[i] = data[i];
-//     }
-
-//     while (true)
-//     {
-//         // Clear the clusters for the next iteration
-//         for (auto &cluster : clusters)
-//         {
-//             cluster.clear();
-//         }
-
-//         // 2. Create a KDTree with these centroids
-//         KDTree<Point> kdtree(centroids);
-
-//         // 3. For each point in the dataset, find the nearest centroid
-//         for (const Point &point : data)
-//         {
-//             int idx = kdtree.knnSearch(point, 1)[0];
-//             clusters[idx].push_back(point);
-//         }
-
-//         // 4. For each cluster, calculate the new centroid
-//         std::vector<Point> newCentroids(k);
-//         for (int i = 0; i < k; ++i)
-//         {
-//             Point centroid;
-//             for (const Point &point : clusters[i])
-//             {
-//                 centroid[0] += point[0];
-//                 centroid[1] += point[1];
-//             }
-//             centroid[0] /= clusters[i].size();
-//             centroid[1] /= clusters[i].size();
-//             newCentroids[i] = centroid;
-//         }
-
-//         // 5. Check if centroids have changed significantly
-//         double diff = 0;
-//         for (int i = 0; i < k; ++i)
-//         {
-//             diff += distance(centroids[i], newCentroids[i]);
-//         }
-
-//         centroids = newCentroids;
-
-//         // 6. Repeat until the centroids do not change significantly
-//         if (diff < 1e-5)
-//         {
-//             break;
-//         }
-//     }
-
-//     return {centroids, clusters};
-// }
-
 std::pair<std::vector<Point>, std::vector<std::vector<Point>>> kdkmeans(const std::vector<Point> &data, int k)
 {
     // 1. Choose k random points as initial centroids
@@ -158,7 +92,7 @@ std::pair<std::vector<Point>, std::vector<std::vector<Point>>> kdkmeans(const st
         centroids[i] = data[i];
     }
 
-    int maxIterations = 1000; // Set a maximum number of iterations
+    int maxIterations = 10; // Set a maximum number of iterations
 
     for (int iteration = 0; iteration < maxIterations; ++iteration)
     {
