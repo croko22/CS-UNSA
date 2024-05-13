11282726bfd8172c48a3f7dc1f34f17bfb70eec8
croko22 <kvchambit@gmail.com>
2024-05-13 11:24:05 -0500

    Sistema experto

---
+++ 6 IA/PROLOG/SistemaExperto/k2.pl
@@ -0,0 +1,13 @@
+% Base de conocimiento para el sistema de diagnóstico automotriz
+diagnostico('Banda del alternador defectuosa', 
+    ['Tiene grietas', 'Está cristalizada', 'Está floje']).
+diagnostico('Banda del alternador defectuosa', 
+    ['La banda está engrasada', 'Rechina al girar']).
+diagnostico('Bateria defectuosa', 
+    ['Luces y ventilador encendidos', 'Motor apagado', 'Voltaje de bateria menor a 10.5 Volts']).
+diagnostico('Regulador defectuoso', 
+    ['Motor en marcha', 'Luces encendidas', 'Se intensifican al acelerar']).
+diagnostico('Alternador defectuoso', 
+    ['La batería está en buenas condiciones', 'Luz de advertencia permanece encendida', 'Motor de arranque gira lentamente']).
+diagnostico('Sistema de frenos defectuoso', 
+    ['Pedal del freno duro', 'Al frenar se produce un tirón lateral', 'Ruedas rechinan al frenar']).
---
+++ 6 IA/PROLOG/SistemaExperto/kb3.pl
@@ -0,0 +1,16 @@
+% Base de conocimiento para el sistema de diagnóstico de animales
+% Reglas de diagnóstico y síntomas
+diagnostico('cheeta', 
+    ['Es mamífero', 'Es carnívoro', 'Tiene color leonado', 'Tiene puntos negros']).
+diagnostico('tigre', 
+    ['Es mamífero', 'Es carnívoro', 'Tiene color leonado', 'Tiene rayas negras']).
+diagnostico('jirafa', 
+    ['Es ungulado', 'Tiene cuello largo', 'Tiene piernas largas']).
+diagnostico('zebra', 
+    ['Es ungulado', 'Tiene rayas negras']).
+diagnostico('aveztruz', 
+    ['Es un pájaro', 'No vuela', 'Tiene cuello largo']).
+diagnostico('pingüino', 
+    ['Es un pájaro', 'No vuela', 'Sabe nadar', 'Es blanco con negro']).
+diagnostico('albatros', 
+    ['Es un pájaro', 'Aparece en historias marinas', 'Vuela bien']).
